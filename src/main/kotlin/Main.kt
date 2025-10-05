5a7a8c0eb26228cddaf2c204f1f46ff88d78be4e
Хомякова Светлана <C:\Users\khomyakova\AppData\Roaming\The Bat!>
2025-10-05 18:28:12 +0300

    Удалила закомментированный код и добавила в тест для WallSevice пост с вложениями

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,5 @@
 package ru.netology
 
-import ru.netology.Coordinates
-import ru.netology.Place
-import kotlin.Boolean
-import kotlin.Int
-import kotlin.String
 
 fun main() {
     val post = Post(
@@ -21,8 +16,8 @@ fun main() {
                 photo = Photo(
                     id = 1,
                     ownerId = 1,
-                    photo130 = "https://vk.com/some_photo_link",
-                    photo604 = "https://vk.com/another_photo_link"
+                    photo130 = "https://terrarium-online.com/wp-content/uploads/2023/10/ushastyj-bananoed-465x260.jpg",
+                    photo604 = "https://terrarium-online.com/wp-content/uploads/2023/10/ushastyj-bananoed-1.jpg"
                 )
             ),
             VideoAttachment(
@@ -31,44 +26,44 @@ fun main() {
                     ownerId = 1,
                     title = "Cats",
                     description = "Funny cats",
-                    duration =30,
+                    duration = 30,
                     views = 2_900_000,
                     player = "https://vkvideo.ru/video-158579430_456262028"
                 )
             ),
             StickerAttachment(
                 sticker = Sticker(
-                    productId = 1, //Идентификатор набора
-                    stickerId = 1, // Идентификатор стикера
-                    animationUrl = "https://vk.com/some_sticker_link", // URL анимации стикера
-                    isAllowed = true // Информация о том, доступен ли стикер.
+                    productId = 1610,
+                    stickerId = 76072,
+                    animationUrl = "https://vk.com/sticker/3-76072.json",
+                    isAllowed = true
                 )
             ),
             FileAttachment(
                 file = File(
                     id = 1,
                     owner_id = 1, // идентификатор владельца видео
-                    title = "My cat", // название
-                    size = 2300, //Размер файла в байтах.
-                    ext = "jpeg", // Расширение файла.
-                    url = "https://vk.com/some_file_address", // Адрес файла, по которому его можно загрузить.
-                    date = 1759092958, // Дата добавления в формате Unixtime
+                    title = "Abronia graminea", // название
+                    size = 98_304, //Размер файла в байтах.
+                    ext = "webp", // Расширение файла.
+                    url = "https://terrarium-online.com/wp-content/uploads/2023/02/abronia-graminea.webp", // Адрес файла, по которому его можно загрузить.
+                    date = 1759661150, // Дата добавления в формате Unixtime
                 )
             ),
             GeotagAttachment(
                 geotag = Geotag(
-                    type = PlaceType.JOB, //Тип места.
+                    type = PlaceType.SIGHT,
                     coordinates = Coordinates(
-                        latitude = 47.456293,
-                        longitude = 40.059049
+                        latitude = 47.416481,
+                        longitude = 40.086535
                     ),
                     place = Place(
-                        type = 1, //тип чекина
-                        groupId = 1, //идентификатор сообщества
-                        groupPhoto = "https://vk.com/mini-img", //URL миниатюры главной фотографии сообщества
-                        checkins = 19, //количество чекинов
-                        updated = 1759092958, // время последнего чекина в Unixtime;
-                        address = 334 // адрес.
+                        type = 1,
+                        groupId = 996167,
+                        groupPhoto = "https://sun9-25.userapi.com/s/v1/ig2/x9jACJlLoXrVuUOehcbNBhHZVBKAm2pq9CzJoDsSkJMO1vAk6qQ8uU4mda_oIc7dUW7N6yxlJZiGQINeTNL8SArB.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280,1300x1300&from=bu&cs=1280x0",
+                        checkins = 10,
+                        updated = 2,
+                        address = 423
                     )
                 )
             ),
@@ -95,167 +90,5 @@ fun main() {
         isFavorite = true
     )
 
-//    val repost = Post (
-//            id = 0,
-//    ownerId = 20,
-//    fromId = 5,
-//    date = 1759093999,
-//    text = "Post 2 content.",
-//    replyOwnerId = 3,
-//    replyPostId = 7,
-//    friendsOnly = false,
-//    comments = Comments(
-//        count = 0,
-//        canPost = false,
-//        groupsCanPost = true
-//    ),
-//    likes = Likes(
-//        count = 4,
-//        userLikes = false,
-//        canLike = false,
-//        canPublish = false
-//    ),
-//    reposts = Reposts(
-//        count = 0,
-//        userReposted = false
-//    ),
-//    views = Views(
-//        count = 10
-//    ),
-//    postType = PostType.COMMENT,
-//    isFavorite = false
-//    )
-//
-//    val testPost3 = Post(
-//        id = 0,
-//        ownerId = 30,
-//        fromId = 8,
-//        date = 1759094999,
-//        text = "Post 3 content.",
-//        replyOwnerId = 4,
-//        replyPostId = 9,
-//        friendsOnly = true,
-//        comments = Comments(
-//            count = 1,
-//            canPost = true,
-//            groupsCanPost = false
-//        ),
-//        likes = Likes(
-//            count = 5,
-//            userLikes = true,
-//            canLike = true,
-//            canPublish = false
-//        ),
-//        reposts = Reposts(
-//            count = 2,
-//            userReposted = false
-//        ),
-//        views = Views(
-//            count = 50
-//        ),
-//        postType = PostType.REPLY,
-//        isFavorite = true
-//    )
-//    val testPost4 = Post(
-//        id = 0,
-//        ownerId = 40,
-//        fromId = 12,
-//        date = 1759095999,
-//        text = "Post 4 content.",
-//        replyOwnerId = 5,
-//        replyPostId = 11,
-//        friendsOnly = false,
-//        comments = Comments(
-//            count = 7,
-//            canPost = false,
-//            groupsCanPost = true
-//        ),
-//        likes = Likes(
-//            count = 8,
-//            userLikes = false,
-//            canLike = true,
-//            canPublish = true
-//        ),
-//        reposts = Reposts(
-//            count = 3,
-//            userReposted = true
-//        ),
-//        views = Views(
-//            count = 200
-//        ),
-//        postType = PostType.POSTPONE,
-//        isFavorite = false
-//    )
-//
-//    val testPost5 = Post(
-//        id = 0,
-//        ownerId = 50,
-//        fromId = 15,
-//        date = 1759096999,
-//        text = "Post 5 content.",
-//        replyOwnerId = 6,
-//        replyPostId = 13,
-//        friendsOnly = true,
-//        comments = Comments(
-//            count = 2,
-//            canPost = true,
-//            groupsCanPost = false
-//        ),
-//        likes = Likes(
-//            count = 12,
-//            userLikes = true,
-//            canLike = true,
-//            canPublish = false
-//        ),
-//        reposts = Reposts(
-//            count = 1,
-//            userReposted = false
-//        ),
-//        views = Views(
-//            count = 75
-//        ),
-//        postType = PostType.SUGGEST,
-//        isFavorite = true
-//    )
-//
-//    val testPost6 = Post(
-//        id = 0,
-//        ownerId = 30,
-//        fromId = 8,
-//        date = 1759095011,
-//        text = "Post 3 content changed.",
-//        replyOwnerId = 12,
-//        replyPostId = 41,
-//        friendsOnly = false,
-//        comments = Comments(
-//            count = 1,
-//            canPost = true,
-//            groupsCanPost = false
-//        ),
-//        likes = Likes(
-//            count = 5,
-//            userLikes = true,
-//            canLike = true,
-//            canPublish = false
-//        ),
-//        reposts = Reposts(
-//            count = 2,
-//            userReposted = false
-//        ),
-//        views = Views(
-//            count = 50
-//        ),
-//        postType = PostType.REPLY,
-//        isFavorite = true
-//    )
-
     println(WallService.add(post))
-//    println(WallService.add(repost))
-//    println(WallService.add(testPost3))
-//    println(WallService.add(testPost4))
-//    println(WallService.add(testPost5))
-
-//    WallService.update(testPost6);
-
-
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -5,10 +5,23 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import ru.netology.Comments
+import ru.netology.Coordinates
+import ru.netology.File
+import ru.netology.FileAttachment
+import ru.netology.Geotag
+import ru.netology.GeotagAttachment
 import ru.netology.Likes
+import ru.netology.Photo
+import ru.netology.PhotoAttachment
+import ru.netology.Place
+import ru.netology.PlaceType
 import ru.netology.Post
 import ru.netology.PostType
 import ru.netology.Reposts
+import ru.netology.Sticker
+import ru.netology.StickerAttachment
+import ru.netology.Video
+import ru.netology.VideoAttachment
 import ru.netology.Views
 import ru.netology.WallService
 import java.util.stream.Stream
@@ -44,6 +57,95 @@ class WallServiceTest {
                     ),
                     views = Views(
                         count = 10
+                    ),
+                    postType = PostType.POST,
+                    isFavorite = true
+                ), 0
+            ),
+            Arguments.of(
+                Post(
+                    id = 0,
+                    ownerId = 10,
+                    fromId = 2,
+                    date = 1759092958,
+                    text = "Post 1 content.",
+                    replyOwnerId = 2,
+                    replyPostId = 5,
+                    friendsOnly = true,
+                    attachments = listOf(
+                        PhotoAttachment(
+                            photo = Photo(
+                                id = 1,
+                                ownerId = 1,
+                                photo130 = "https://terrarium-online.com/wp-content/uploads/2023/10/ushastyj-bananoed-465x260.jpg",
+                                photo604 = "https://terrarium-online.com/wp-content/uploads/2023/10/ushastyj-bananoed-1.jpg"
+                            )
+                        ),
+                        VideoAttachment(
+                            video = Video(
+                                id = 1,
+                                ownerId = 1,
+                                title = "Cats",
+                                description = "Funny cats",
+                                duration = 30,
+                                views = 2_900_000,
+                                player = "https://vkvideo.ru/video-158579430_456262028"
+                            )
+                        ),
+                        StickerAttachment(
+                            sticker = Sticker(
+                                productId = 1610,
+                                stickerId = 76072,
+                                animationUrl = "https://vk.com/sticker/3-76072.json",
+                                isAllowed = true
+                            )
+                        ),
+                        FileAttachment(
+                            file = File(
+                                id = 1,
+                                owner_id = 1, // идентификатор владельца видео
+                                title = "Abronia graminea", // название
+                                size = 98_304, //Размер файла в байтах.
+                                ext = "webp", // Расширение файла.
+                                url = "https://terrarium-online.com/wp-content/uploads/2023/02/abronia-graminea.webp", // Адрес файла, по которому его можно загрузить.
+                                date = 1759661150, // Дата добавления в формате Unixtime
+                            )
+                        ),
+                        GeotagAttachment(
+                            geotag = Geotag(
+                                type = PlaceType.SIGHT,
+                                coordinates = Coordinates(
+                                    latitude = 47.416481,
+                                    longitude = 40.086535
+                                ),
+                                place = Place(
+                                    type = 1,
+                                    groupId = 996167,
+                                    groupPhoto = "https://sun9-25.userapi.com/s/v1/ig2/x9jACJlLoXrVuUOehcbNBhHZVBKAm2pq9CzJoDsSkJMO1vAk6qQ8uU4mda_oIc7dUW7N6yxlJZiGQINeTNL8SArB.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1080x1080,1280x1280,1300x1300&from=bu&cs=1280x0",
+                                    checkins = 10,
+                                    updated = 2,
+                                    address = 423
+                                )
+                            )
+                        ),
+                    ),
+                    comments = Comments(
+                        count = 3,
+                        canPost = true,
+                        groupsCanPost = false
+                    ),
+                    likes = Likes(
+                        count = 2,
+                        userLikes = true,
+                        canLike = true,
+                        canPublish = true
+                    ),
+                    reposts = Reposts(
+                        count = 4,
+                        userReposted = true
+                    ),
+                    views = Views(
+                        count = 123
                     ),
                     postType = PostType.POST,
                     isFavorite = true
